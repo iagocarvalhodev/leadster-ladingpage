a0a820101c3c255e16e7483ce5b8cce0c026aa79
Iago Carvalho <iagocarvalhoweb@gmail.com>
2023-06-23 13:06:39 -0300

    added lint, prettier and jest configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import "./globals.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import './globals.css'
+import { Plus_Jakarta_Sans } from 'next/font/google'
 
-const Jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const Jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "LeadSter",
-  description: "Teste de Desenvolvimento",
-};
+  title: 'LeadSter',
+  description: 'Teste de Desenvolvimento',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={Jakarta.className}>{children}</body>
     </html>
-  );
+  )
 }
